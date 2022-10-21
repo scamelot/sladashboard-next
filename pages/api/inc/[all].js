7ea01579e8eef88bde5a96e36ed5e1041484ee95
Steve Scammon <stevescammon@me.com>
2022-10-21 10:26:33 -0400

    last 30 days

--- pages/api/inc/[all].js
+++ pages/api/inc/[all].js
@@ -14,11 +14,17 @@ export default async function handler(req,res) {
 
    const allIncSlas = await db
     .collection("inc")
-    .find({})
+    .find({"date": 
+    {
+        $gte: new Date((new Date().getTime() - (30 * 24 * 60 * 60 * 1000)))
+    }})
     .toArray()
   const allReqSlas = await db
     .collection("req")
-    .find({})
+    .find({"date": 
+    {
+        $gte: new Date((new Date().getTime() - (30 * 24 * 60 * 60 * 1000)))
+    }})
     .toArray()
 
    const data = {
