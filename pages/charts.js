0aa2e42786a40795b24e15235326fc46212ad164
Steve Scammon <stevescammon@me.com>
2022-10-21 13:34:36 -0400

    chart colors and annotations

--- pages/charts.js
+++ pages/charts.js
@@ -1,5 +1,10 @@
-import { Chart } from 'chart.js/auto'
+// import Chart from 'chart.js/auto';
+import Annotation from 'chartjs-plugin-annotation';
+// Global
 import { Line } from 'react-chartjs-2'
+import { Chart as ChartJS, ArcElement, CategoryScale, LinearScale, PointElement, LineElement, Filler, Tooltip, Legend, ScriptableContext, } from "chart.js";
+ChartJS.register(ArcElement, Annotation, Tooltip, Filler, Legend, CategoryScale, LinearScale, PointElement, LineElement);
+
 
 export default function IncChart(props) {
     const allData = props.data
@@ -8,60 +13,169 @@ export default function IncChart(props) {
     let incData = []
     let incLabels = []
     allData.inc.forEach(datum => {
-        incLabels.push(datum.date)
+        incLabels.push(datum.date.replace('-2022',''))
         incData.push(datum.value)
     })
 
     let reqData = []
     let reqLabels = []
     allData.req.forEach(datum => {
-        reqLabels.push(datum.date)
+        reqLabels.push(datum.date.replace('-2022', ''))
         reqData.push(datum.value)
     })
 
-    const data = {
-            labels: incLabels,
-            datasets: [{
-                label: 'INC Percentage',
-                data: incData,
-                lineTension: 0.1,
-                borderCapStyle: 'butt',
-                borderJoinStyle: 'miter',
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                ],
-                borderWidth: 2,
-            },
-            {
+    const reqChartData = {
+            labels: reqLabels,
+            datasets:[{
                 label: 'REQ Percentage',
                 data: reqData,
-                lineTension: 0.1,
+                lineTension: 0.4,
                 borderCapStyle: 'butt',
                 borderJoinStyle: 'miter',
                 backgroundColor: [
                     'rgba(67, 118, 197, 0.2)',
                 ],
                 borderColor: [
-                    'rgba(67, 118, 197, 1)',
+                    'rgba(0, 253, 255, 1)',
                 ],
                 borderWidth: 2,
             }]
+    }
+
+    const data = {
+            labels: incLabels,
+            datasets: [{
+                label: 'INC Percentage',
+                data: incData,
+                lineTension: 0.4,
+                borderCapStyle: 'butt',
+                borderJoinStyle: 'miter',
+                backgroundColor: [
+                    'rgba(0, 255, 132, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(0, 255, 0, 1)',
+                ],
+                borderWidth: 2,
+            }],
         }
 
-    const options = {
+    const reqOptions = {
         scales: {
-        yAxes: [{
-            ticks: {
-                beginAtZero: true
+            x: {
+                grid: {
+                    color: 'rgba(100,100,100,0.4)'
+                }
+            },
+            y: {
+                grid: {
+                    color: 'rgba(100,100,100,0.4)'
+                },
+                min: 95,
+            },
+        },
+        maintainAspectRatio: false,
+        plugins: {
+            animation: {
+                duration: 5
+            },
+        annotation: {
+            annotations: [{
+            type: 'box',
+            mode: 'horizontal',
+            xMin: -1,
+            xMax: 99,
+            yMin: 96.219,
+            yMax: 97.161,
+            backgroundColor: 'rgba(255,180,0,0.4)',
+            scaleID: 'y',
+            value: 97.160,
+            borderColor: 'rgb(255, 255, 0, 0.4)',
+            borderWidth: 1,
+            label: {
+                enabled: false,
+                content: 'Req Warning',
+            },
+        },
+        {
+            type: 'box',
+            mode: 'horizontal',
+            xMin: -1,
+            xMax: 99,
+            yMin: 0,
+            yMax: 96.219,
+            backgroundColor: 'rgba(255,0,0,0.4)',
+            value: 96.219,
+            borderWidth: 1,
+            label: {
+                enabled: false,
+                content: 'Req Failure',
             }
-        }]
-    }}
+        }],
+    }}}
+
+    const options = {
+            scales: {
+                x: {
+                    grid: {
+                        color: 'rgba(100,100,100,0.4)'
+                    }
+                },
+                y: {
+                    grid: {
+                        color: 'rgba(100,100,100,0.4)'
+                    },
+                    min: 90,
+                },
+            },
+    maintainAspectRatio: false,
+    plugins: {
+        animation: {
+            duration: 5
+        },
+        annotation: {
+            annotations: [{
+                type: 'box',
+            mode: 'horizontal',
+            xMin: -1,
+            xMax: 99,
+            yMin: 92.895,
+            yMax: 94.850,
+            backgroundColor: 'rgba(255,180,0,0.4)',
+                value: 94.850,
+                borderColor: 'rgb(255, 255, 0, 0.4)',
+                borderWidth: 1,
+                label: {
+                    enabled: false,
+                    content: 'INC Warning',
+                }
+            },
+            {
+                type: 'box',
+            mode: 'horizontal',
+            xMin: -1,
+            xMax: 99,
+            yMin: 0,
+            yMax: 92.895,
+            backgroundColor: 'rgba(255,0,0,0.4)',
+                value: 92.894,
+                borderColor: 'rgb(255, 0, 0, 0.4)',
+                borderWidth: 1,
+                label: {
+                    enabled: false,
+                    content: 'INC Failure',
+                }
+            },
+            ],
+            drawTime: 'afterDraw'
+        }
+    }  
+}
+
     return (
-        <div style={{width: '50vw', height: 300}}>
+        <div style={{display: 'flex', marginLeft:'-45vw', justifyContent: 'between',flexDirection:'row',zIndex:'0', left: '100px', bottom:'100px', width: '45vw', height: 400}}>
         <Line data={data} width={'100px'} height={'100px'} options={options} />
+        <Line data={reqChartData} options={reqOptions} />
         </div>
     )
 }
