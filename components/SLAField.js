387d01e5ba877e204f7dab9292e6d47b884b6c9e
Steve Scammon <stevescammon@me.com>
2022-12-01 18:20:54 -0500

    color fix

--- components/SLAField.js
+++ components/SLAField.js
@@ -1,6 +1,5 @@
 import styles from '../styles/Home.module.css'
 import {incLimits, reqLimits} from '../config/limits'
-import {colors} from '../config/colors'
 
 export default function SLAField(props) {
 
@@ -8,8 +7,6 @@ export default function SLAField(props) {
         low: " to goal",
         high: " above goal"
     }
-    console.log(incLimits)
-    console.log(colors)
     const incValues = incLimits
     const reqValues = reqLimits
     let toGoal = 0
@@ -27,22 +24,21 @@ export default function SLAField(props) {
         if (props.name.includes('INCs')) {
             values = incValues
         } else { values = reqValues }
-
         toGoal = Math.abs(values[1] - value).toFixed(3)
 
         if (value < values[0]) {
             goalLabel = goalText.low
-            return colors.BAD
+            return 'bg-red-500'
 
         }
         else if (value >= values[0] &&  value < values[1]) {
             goalLabel = goalText.low
-            return colors.OKAY
+            return 'bg-yellow-500'
 
         }
         else {
             goalLabel = goalText.high
-            return colors.GOOD
+            return 'bg-green-700'
 
         }    
     }
@@ -73,8 +69,8 @@ export default function SLAField(props) {
     return (
         <a href="#" className={styles.card}>
         <h2 className="text-center lg:text-5xl sm:text-lg">{props.name}</h2>
-        <input readOnly={true} type="text" id="INCs" className={`lg:text-8xl md:text-7xl sm:text-6xl rounded-lg text-center text-white w-3/4 h-5/6 m-5 ${fieldStyle}`} value={props.value}></input>
-        <h3 id='delta' className={`text-center lg:text-4xl sm:text-md ${deltaColor}`}>Change since {`${props.prev}`}: {`${delta}%`}</h3>
+        <input readOnly={true} type="text" id="INCs" className={`${fieldStyle} lg:text-8xl md:text-7xl sm:text-6xl rounded-lg text-center text-white w-3/4 h-5/6 m-5`} value={props.value}></input>
+        <h3 id='delta' className={`text-center lg:text-4xl sm:text-md`}>Change since {`${props.prev}`}: <span className={deltaColor}>{`${delta}%`}</span></h3>
         <h2 id='goal' className='text-center lg:text-6xl sm:text-lg'>{`${toGoal}`}% {`${goalLabel}`}</h2>
         <h2 id='failure' className='text-center lg:text-4xl sm:text-sm mb-5' > {`${failure}% above minimum`}</h2>
         </a>
--- config/colors.js
+++ config/colors.js
@@ -1,5 +1,22 @@
 export const colors = {
-    GOOD:'bg-green-700',
-    OKAY:'bg-yellow-500',
-    BAD:'bg-red-600'
+    good:'bg-green-700',
+    okay:'bg-yellow-500',
+    bad:'bg-red-600'
+}
+
+export const chartColors = {
+    inc: {
+        line: 'rgba(0,253,255,0.2)',
+        border: 'rgba(0,255,0,1)'
+    },
+    req: {
+        line: '',
+        border: '',
+    },
+    warning: {
+        background: ''
+    },
+    failure: {
+        background: ''
+    }
 }
