c93bb4f6623259493286328a31b4418bc4e33d54
Steve Scammon <stevescammon@me.com>
2022-12-02 11:56:06 -0500

    mobile formatting

--- components/SLACard.js
+++ components/SLACard.js
@@ -3,7 +3,7 @@ import SLAChart from "./charts";
 
 export default function SLACard({name, current, prev, data}) {
     return(
-        <div className='flex flex-row'>
+        <div className='flex md:flex-row sm:flex-col'>
         <SLAField name={name} value={current.value.toString()} prev={prev.date} prevValue={prev.value.toString()} />
         <SLAChart name={name} data={data} />
         </div>
