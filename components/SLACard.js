dd76f7898d3c292d7ad4daec6a7c946b784ad28c
Steve Scammon <stevescammon@me.com>
2023-01-03 09:21:29 -0500

    protect against null values getting inserted into db

--- components/SLACard.js
+++ components/SLACard.js
@@ -4,7 +4,7 @@ import SLAChart from "./charts";
 export default function SLACard({name, current, prev, data}) {
     return (
         <div className='flex md:flex-row sm:flex-col'>
-        <SLAField name={name} value={current.value.toString()} prev={prev.date} prevValue={prev.value.toString()} />
+        <SLAField name={name} value={current.value ? current.value.toString() : '--'} prev={prev.date} prevValue={prev.value ? prev.value.toString() : '--'} />
         <SLAChart name={name} data={data} />
         </div>
     )
