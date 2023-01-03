2d348758512069ceec32f5c43335aa8145fe44d9
Steve Scammon <stevescammon@me.com>
2023-01-03 10:09:49 -0500

    protect against null values getting inserted into db

--- pages/api/inc/auto.js
+++ pages/api/inc/auto.js
@@ -23,7 +23,7 @@ export default async function handler (req, res) {
                 date: today,
                 type: req.body.type,
                 dateReadable: today,
-                value: isNaN(realValue) ? realValue : 100,
+                value: isNaN(realValue) ? 100 : realValue,
             }},
             {
                 upsert: true
